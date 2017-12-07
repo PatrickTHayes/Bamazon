0f57f63c04bfbe86991c8cc27a8d0cc12b8c4506
Pat hayes <pat2382@yahoo.com>
2017-12-07 13:43:29 +0000

    schema with seeds finished

---
+++ bamazonSeeds.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+
+USE bamazon;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(45) NULL,
+  department_name VARCHAR(45) NULL,
+  price decimal(8,2) NULL,
+  stock_quantity INT(10) NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("PS4","Electronics", 250, 10), ("Windows PC", "Electronics", 300,10), ("MacBook Pro", "Electronics", 1200,2),("XBOX 1","Electronics",250,10);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("apple","Produce", 1.5, 100), ("orange", "Produce", 2,150), ("celery", "Produce", 1,200),("carrots","Produce",2,150);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("desk","Furniture", 599.99, 5), ("lamp", "Furniture", 75.50,11);
